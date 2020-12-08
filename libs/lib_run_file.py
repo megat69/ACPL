7df4340c19a2cf4e6f41fc0ea5f6309ab201bc5a
TheAssassin <47896737+megat69@users.noreply.github.com>
2020-12-08 18:13:04 +0100

    Delete lib_run_file.py

--- libs/lib_run_file.py
+++
@@ -1,9 +0,0 @@
-version = 1.0
-lib_type = "functions"
-import os
-from recurrent_classes import replace_line
-
-if var_line.startswith("run_file"):
-    var = var_line.replace("run_file ", "")
-    replace_line("startup.acpl-ini", 0, f"filename: {var}\n")
-    os.system("python main.py")
